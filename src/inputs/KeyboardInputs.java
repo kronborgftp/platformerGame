62dadfe7d9bf3cc805327d5c3efc47e2c511c352
kronborgftp <frpa0001@stud.kea.dk>
2023-09-20 15:57:08 +0200

    player class and attack animation

--- src/inputs/KeyboardInputs.java
+++ src/inputs/KeyboardInputs.java
@@ -1,59 +1,57 @@
 package inputs;
 
-import main.GamePanel;
-
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
+import main.GamePanel;
 import static utils.Constants.Directions.*;
 
 public class KeyboardInputs implements KeyListener {
 
     private GamePanel gamePanel;
-    public KeyboardInputs(GamePanel gamepanel){
-        this.gamePanel = gamepanel;
+
+    public KeyboardInputs(GamePanel gamePanel) {
+        this.gamePanel = gamePanel;
     }
+
     @Override
     public void keyTyped(KeyEvent e) {
-
-    }
-
-    @Override
-    public void keyPressed(KeyEvent e) {
-
-        switch(e.getKeyCode()) {
-
-        case KeyEvent.VK_W:
-            gamePanel.setDirection(UP);
-            break;
-        case KeyEvent.VK_A:
-            gamePanel.setDirection(LEFT);
-            break;
-        case KeyEvent.VK_S:
-            gamePanel.setDirection(DOWN);
-            break;
-        case KeyEvent.VK_D:
-            gamePanel.setDirection(RIGHT);
-            break;
-
-    }
-
-
+        // TODO Auto-generated method stub
     }
 
     @Override
     public void keyReleased(KeyEvent e) {
-
-        switch(e.getKeyCode()) {
-
+        switch (e.getKeyCode()) {
             case KeyEvent.VK_W:
-            case KeyEvent.VK_A:
-            case KeyEvent.VK_S:
-            case KeyEvent.VK_D:
-                gamePanel.setMoving(false);
-
+                gamePanel.getGame().getPlayer().setUp(false);
                 break;
+            case KeyEvent.VK_A:
+                gamePanel.getGame().getPlayer().setLeft(false);
+                break;
+            case KeyEvent.VK_S:
+                gamePanel.getGame().getPlayer().setDown(false);
+                break;
+            case KeyEvent.VK_D:
+                gamePanel.getGame().getPlayer().setRight(false);
+                break;
+        }
+    }
 
+    @Override
+    public void keyPressed(KeyEvent e) {
+        switch (e.getKeyCode()) {
+            case KeyEvent.VK_W:
+                gamePanel.getGame().getPlayer().setUp(true);
+                break;
+            case KeyEvent.VK_A:
+                gamePanel.getGame().getPlayer().setLeft(true);
+                break;
+            case KeyEvent.VK_S:
+                gamePanel.getGame().getPlayer().setDown(true);
+                break;
+            case KeyEvent.VK_D:
+                gamePanel.getGame().getPlayer().setRight(true);
+                break;
         }
     }
 }
--- src/main/Game.java
+++ src/main/Game.java
@@ -1,5 +1,9 @@
 package main;
 
+import entities.Player;
+
+import java.awt.*;
+
 public class Game implements Runnable {
 
     private GameWindow gameWindow;
@@ -8,13 +12,21 @@ public class Game implements Runnable {
     private final int FPS_SET = 120;
     private final int UPS_SET = 200;
 
-    public Game() {
+    private Player player;
 
-        gamePanel = new GamePanel();
+    public Game() {
+        initClasses();
+
+        gamePanel = new GamePanel(this);
         gameWindow = new GameWindow(gamePanel);
         gamePanel.requestFocus();
+
         startGameLoop();
 
+    }
+
+    private void initClasses() {
+        player = new Player(200, 200);
     }
 
     private void startGameLoop() {
@@ -23,7 +35,11 @@ public class Game implements Runnable {
     }
 
     public void update() {
-        gamePanel.updateGame();
+        player.update();
+    }
+
+    public void render(Graphics g) {
+        player.render(g);
     }
 
     @Override
@@ -71,7 +87,15 @@ public class Game implements Runnable {
                 updates = 0;
             }
         }
-
     }
+
+    public void windowFocusLost() {
+        player.resetDirBooleans();
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
 
 }
